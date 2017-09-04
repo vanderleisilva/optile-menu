cb555bbb4dbeac18fd75def067fc287042812e79
Vanderlei Alves da Silva <vanderleisilva@santos.sp.gov.br>
2017-09-04 11:05:58 -0300

    Basic html layout

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container-fluid">
+        <div className="page-header">
+          <h1>Optile Menu <small className="hidden-sm">Optile react.js coding challenge</small></h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="thumbnail">
+          
+          <img src="..." alt="..." />
+          <div className="caption">
+            <p>See what we have for lunch without leave your seat</p>
+            <p>
+              <a href="#" className="btn btn-primary" title="Update Menu" role="button">
+                Update <span className="glyphicon glyphicon-upload" aria-hidden="true"></span>
+              </a> 
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
